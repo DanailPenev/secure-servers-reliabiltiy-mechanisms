fab2455e58df15471d72c48fc8d12af1e33ef351
Danail Penev <danailnpenev@gmail.com>
2019-03-07 14:44:30 +0000

    Fix interface name

--- src/main/scala/server/WebServer.scala
+++ src/main/scala/server/WebServer.scala
@@ -15,7 +15,7 @@ object WebServer extends App with Routes {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
